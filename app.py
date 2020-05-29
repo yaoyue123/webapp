e7353c17941fead2e40ecb7655a64af03f25508c
yaoyue123 <2742949404@qq.com>
2020-05-29 16:06:36 +0800

    change to use flask_sqlalchemy

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators, FloatField
 from passlib.hash import sha256_crypt
 from functools import wraps
@@ -9,22 +8,15 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.debug = True
 
-# Config MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'admin123'
-app.config['MYSQL_DB'] = 'webapp'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-# init MYSQL
-mysql = MySQL(app)
+#init db
 app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@localhost/flask'
+    'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@localhost/webapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 
-class user_admin(db.Model):
+class User_admin(db.Model):
     __tablename__ = 'user_admin'
     school_num = db.Column(db.String(13), primary_key=True)
     password = db.Column(db.Text, nullable=False)
@@ -39,11 +31,8 @@ class user_admin(db.Model):
         self.name = name
         self.phone = phone
 
-    def __repr__(self):
-        return '<school_num %r>' % self.school_num
 
-
-class user_data(db.Model):
+class User_data(db.Model):
     __tablename__ = 'user_data'
     id = db.Column(db.Integer,
                    autoincrement=True,
@@ -55,14 +44,10 @@ class user_data(db.Model):
     temperature = db.Column(db.Float, nullable=False)
     date = db.Column(db.Date, nullable=False)
 
-    def __init__(self, school_num, password, rank, name, phone):
-        self.id = id
+    def __init__(self, school_num, temperature, date):
         self.school_num = school_num
         self.temperature = temperature
         self.date = date
-
-    def __repr__(self):
-        return '<school_num %r>' % self.school_num
 
 
 db.create_all()
@@ -81,39 +66,6 @@ def about():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
-
-@app.route('/articles')
-def articles():
-
-    # create cursor
-    cur = mysql.connection.cursor()
-
-    # get articles
-    result = cur.execute("SELECT * FROM articles")
-
-    articles = cur.fetchall()
-
-    if result > 0:
-        return render_template('articles.html', articles=articles)
-    else:
-        msg = 'No Articles Found'
-        return render_template('articles.html', msg=msg)
-    # close connection
-    cur.close()
-
-
-@app.route('/article/<string:id>/')
-def article(id):
-    # create cursor
-    cur = mysql.connection.cursor()
-
-    # get article
-    result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
-
-    article = cur.fetchone()
-
-    return render_template('article.html', article=article)
 
 
 class RegisterForm(Form):
@@ -138,23 +90,16 @@ def register():
         password = sha256_crypt.encrypt(str(form.password.data))
 
         # Create crusor
-        cur = mysql.connection.cursor()
 
-        # 返回成功记录数
-        if cur.execute(
-                "SELECT school_num FROM user_admin WHERE school_num = %s",
-            [school_num]) == 0:
-            cur.execute(
-                "INSERT INTO user_admin(school_num,password,rank,name,phone) VALUES(%s,%s,0,%s,%s)",
-                (school_num, password, name, phone))
-        else:
+        user = User_admin(school_num, password, 0, name, phone)
+        #用User_admin查询，不能用user，会出现永1的情况
+        if User_admin.query.filter(
+                User_admin.school_num == school_num).count() > 0:
             flash("该账号已注册", "warning")
             return redirect(url_for('login'))
-
-        # commit to DB
-        mysql.connection.commit()
-        # close connection
-        cur.close()
+        else:
+            db.session.add(user)
+            db.session.commit()
 
         flash("您的账号已经注册好了，请登录", 'success')
         return redirect(url_for('login'))
@@ -172,39 +117,24 @@ def login():
         school_num = request.form['school_num']
         password_candidate = request.form['password']
 
-        # Create cursor
-
-        cur = mysql.connection.cursor()
-
-        # Get user by username
-
-        result = cur.execute("SELECT * FROM user_admin WHERE school_num = %s",
-                             [school_num])
-
-        if result > 0:
-            # Get Stored hash
-            user = cur.fetchone()
-            password = user['password']
-
-            # Compare Passwords
+        if User_admin.query.filter(
+                User_admin.school_num == school_num).count() > 0:
+            result = User_admin.query.filter(
+                User_admin.school_num == school_num).first()
+            password = result.password
             if sha256_crypt.verify(password_candidate, password):
-                # Passed
                 session['logged_in'] = True
                 session['school_num'] = school_num
-                session['rank'] = user['rank']
-                session['username'] = user['name']
+                session['rank'] = result.rank
+                session['username'] = result.name
                 flash('你已登录', 'success')
                 return redirect(url_for('dashboard'))
             else:
                 error = '密码错误'
                 return render_template('login.html', error=error)
-                # close connection
-            cur.close()
-
         else:
             error = '当前学号不存在，请注册'
             return render_template('login.html', error=error)
-
     return render_template('login.html')
 
 
@@ -252,29 +182,21 @@ def logout():
 @app.route('/dashboard')
 @is_logged_in
 def dashboard():
-
-    # create cursor
-    cur = mysql.connection.cursor()
-
     if session['rank']:
-        result = cur.execute("SELECT * FROM user_data WHERE 1")
-        users = cur.fetchall()
+        users = User_data.query.all()
+        count = User_data.query.count()
     else:
-        result = cur.execute("SELECT * FROM user_data WHERE school_num = %s",
-                             [session['school_num']])
-        users = cur.fetchall()
+        users = User_data.query.filter(
+            User_data.school_num == session['school_num']).all()
+        count = User_data.query.filter(
+            User_data.school_num == session['school_num']).count()
 
-    # get articles
-    #result = cur.execute("SELECT * FROM user_data WHERE 1")
-    #users = cur.fetchall()
-
-    if result > 0:
+    if count > 0:
         return render_template('dashboard.html', users=users)
     else:
-        msg = 'No data Found'
+        msg = '没有记录'
         return render_template('dashboard.html', msg=msg)
-    # close connection
-    cur.close()
+
     return render_template('dashboard.html')
 
 
@@ -282,7 +204,6 @@ def dashboard():
 @is_logged_in
 @is_admin
 def admin_user():
-    cur = mysql.connection.cursor()
 
     return render_template("admin_user.html")
 
@@ -294,38 +215,25 @@ class PunchForm(Form):
     ])
 
 
-# Add Article
-
-
 @app.route('/punch_in', methods=['GET', 'POST'])
 @is_logged_in
 def punch_in():
     form = PunchForm(request.form)
     if request.method == 'POST' and form.validate():
         temperature = form.temperature.data
-        # Create a cursor
+
         date = strftime("%Y-%m-%d", localtime(time()))
-        cur = mysql.connection.cursor()
 
-        # execute
-
-        if cur.execute(
-                "SELECT * FROM user_data WHERE school_num = %s AND date = %s",
-            (session['school_num'], date)):
+        data = User_data(session['school_num'], temperature, date)
+        #组合查询用filter_by
+        if User_data.query.filter_by(school_num=session['school_num'],
+                                     date=date).count():
             flash('今日已打卡', 'warning')
             return redirect(url_for('dashboard'))
         else:
-            cur.execute(
-                "INSERT INTO user_data(school_num,temperature,date) VALUES(%s, %s, %s)",
-                (session['school_num'], temperature, date))
-
-        # commit to db
-
-        mysql.connection.commit()
-
-        # close connection
-        cur.close()
-
+            db.session.add(data)
+            db.session.commit()
+            return redirect(url_for('dashboard'))
         flash('打卡成功 ', 'success')
 
         return redirect(url_for('dashboard'))
@@ -333,50 +241,29 @@ def punch_in():
     return render_template('punch_in.html', form=form)
 
 
-# Edit Article
 @app.route('/edit_info/<string:id>', methods=['GET', 'POST'])
 @is_logged_in
 def edit_info(id):
-    # Create cursor
-    cur = mysql.connection.cursor()
-    # get article by id
+
     if id == session['school_num'] or session['rank']:
         pass
     else:
         flash('权限不够', 'warning')
         return redirect(url_for('dashboard'))
-    result = cur.execute("SELECT * FROM user_admin WHERE school_num = %s",
-                         [id])
 
-    user = cur.fetchone()
+    user = User_admin.query.filter(User_admin.school_num == id).first()
 
-    # get form
     form = RegisterForm(request.form)
 
-    # populate article form fields
-    form.name.data = user['name']
-    form.phone.data = user['phone']
-    form.school_num.data = user['school_num']
+    form.name.data = user.name
+    form.phone.data = user.phone
+    form.school_num.data = user.school_num
 
     if request.method == 'POST' and form.validate():
-        name = request.form['name']
-        phone = request.form['phone']
-        password = sha256_crypt.encrypt(str(request.form['password']))
-        # Create a cursor
-        cur = mysql.connection.cursor()
-        # execute
-
-        cur.execute(
-            "UPDATE user_admin SET name=%s, phone=%s,password = %s WHERE school_num = %s",
-            (name, phone, password, id))
-
-        # commit to db
-
-        mysql.connection.commit()
-
-        # close connection
-        cur.close()
-
+        user.name = request.form['name']
+        user.phone = request.form['phone']
+        user.password = sha256_crypt.encrypt(str(request.form['password']))
+        db.session.commit()
         flash('信息已更改', 'success')
 
         return redirect(url_for('dashboard'))
@@ -384,27 +271,14 @@ def edit_info(id):
     return render_template('edit_info.html', form=form)
 
 
-# Delete article
-
-
 @app.route('/delete_data/<string:school_num>/<string:date>', methods=['POST'])
 @is_logged_in
 @is_admin
 def delete_data(school_num, date):
-    # Create cursor
-    cur = mysql.connection.cursor()
 
-    # Execute
-    cur.execute("DELETE FROM user_data WHERE school_num = %s AND date = %s",
-                (school_num, date))
-
-    # Commit to DB
-
-    mysql.connection.commit()
-    # close connection
-
-    cur.close()
-
+    data = User_data.query.filter_by(school_num=school_num, date=date).first()
+    db.session.delete(data)
+    db.session.commit()
     flash('已删除', 'success')
 
     return redirect(url_for('dashboard'))
